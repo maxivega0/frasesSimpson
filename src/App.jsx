467a785945d0943462cd71581ccaa9a1837793b2
maxivega0 <maximilianovega600@gmail.com>
2023-06-06 16:40:44 -0300

    pagina funcionando

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import logo from "./assets/logosimpson.png";
-import { Button, Container } from "react-bootstrap";
+import { Button, Container, Spinner } from "react-bootstrap";
 import Frase from "./components/Frase";
 import { useEffect, useState } from "react";
 
 function App() {
+  const [mostrarSpinner, setMostrarSpinner] = useState(false);
   const [personaje, setPersonaje] = useState({});
 // Recomendacion, al trabajar con apis solo usar el "montaje" hasta que tenga bien en claro
 // lo que necesito usar
@@ -17,6 +18,7 @@ function App() {
   const consultarAPI = async () =>{
     // Usamos el trycatch para estar notificados si hay un error
     try{
+      setMostrarSpinner(true);
       // Peticion get, solo devuelve datos
       // Siempre que trabajemos con promesas, las promesas tardan un tiempo
       const respuesta = await fetch('https://thesimpsonsquoteapi.glitch.me/quotes');
@@ -25,20 +27,23 @@ function App() {
       console.log(respuesta);
       console.log(dato[0]);
       setPersonaje(dato[0])
+      setMostrarSpinner(false);
 
     } catch(error){
       console.log(error);
     }
   }
 
-
+  const componenteRenderizado = (mostrarSpinner) ? (<div className="my-5">
+  <Spinner animation="border" variant="info" />
+  </div>) : <Frase personaje={personaje}  />
 
   return (
     <>
       <Container className="text-center my-5">
         <img src={logo} alt="Logo de los simpson" className="w-50" />
-        <Frase personaje={personaje} />
-        <Button variant="warning" >
+        {componenteRenderizado}
+        <Button variant="warning" onClick={consultarAPI} >
           Obtener frase
         </Button>
       </Container>
